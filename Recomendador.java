17e9c1c24cdf719457ac7bc4f25d23c12d275d7e
llAkihitoll <saul.esteban.casti@gmail.com>
2025-05-05 23:21:14 -0600

    Add files via upload

---
+++ Recomendador.java
@@ -0,0 +1,8 @@
+public class Recomendador {
+
+    // Método borrador que más adelante conectará con la base de datos en Neo4j
+    public void recomendarCarreras(Usuario usuario) {
+        System.out.println("Recomendando carreras para: " + usuario.getNombre());
+        // Aquí se hará la lógica basada en grafos
+    }
+}
