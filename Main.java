6ee53789829d2811a581281781ad1863a90e4421
Oliver <olivercifuentes338@gmail.com>
2025-05-18 19:38:07 -0600

    Main

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("Daniel", "U001", "Tecnología, lógica", "Matemática, Física");
-        Recomendador r = new Recomendador();
-        r.recomendarCarreras(u1);
+        // Lanzar la GUI
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new InterfazGrafica().setVisible(true);
+        });
     }
 }
