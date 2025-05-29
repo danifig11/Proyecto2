545a77e77a3b44f03dfb0eb24b112592d388bd7d
Oliver <161040825+danifig11@users.noreply.github.com>
2025-05-29 08:00:27 -0600

    Creacion de Neo4jConncetion
    Neo4j Connection

---
+++ Neo4jConnection.java
@@ -0,0 +1,21 @@
+package com.example;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+
+public class Neo4jConnection {
+    private final Driver driver;
+
+    public Neo4jConnection(String uri, String user, String password) {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    public Session getSession() {
+        return driver.session();
+    }
+
+    public void close() {
+        driver.close();
+    }
+}
